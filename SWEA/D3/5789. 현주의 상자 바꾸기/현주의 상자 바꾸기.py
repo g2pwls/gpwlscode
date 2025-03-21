2a1baacd042653eacbfe4ab516b185340680cee3
g2pwls <118176317+g2pwls@users.noreply.github.com>
2025-03-21 15:37:40 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 287 ms, Memory: 65,920 KB -BaekjoonHub

--- SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -1,17 +1,14 @@
-T = int(input())
+T = int(input())  # 테스트 케이스 개수
 
-for tc in range(1, T+1):
-    N, Q = map(int, input().split())
-    # 상자 만들어두기
-    box = [0] * N
+for tc in range(1, T + 1):
+    N, Q = map(int, input().split())  # N: 상자 개수, Q: 작업 개수
+    boxes = [0] * N  # 처음에는 모든 상자가 0
 
-    # Q회 동안 반복
-    for i in range(Q):
-        L, R = map(int, input().split())
+    # Q번의 작업 수행
+    for i in range(1, Q + 1):
+        L, R = map(int, input().split())  # 범위 입력
+        for j in range(L - 1, R):  # 인덱스 조정 (1-based -> 0-based)
+            boxes[j] = i  # i번째 작업 번호로 업데이트
 
-        # 리스트값 수정
-        for bo in range(L-1, R):
-            # i는 0부터 시작하기 때문에 +1 해줘야 함
-            box[bo] = i+1
-
-    print(f"#{tc}", *box)
+    # 결과 출력
+    print(f"#{tc}", *boxes)
